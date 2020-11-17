cb429d2130a9619400f36173dd0d41954cf41672
Fabian <fmentzer@me.com>
2020-11-17 13:16:29 +0100

    Updated Test

--- pypi/test.sh
+++ pypi/test.sh
@@ -3,11 +3,12 @@
 # Test PyPi package
 #
 
+
 TESTS_PATH=$1
 PYTORCH_VERSION=$2
 
 if [[ -z $TESTS_PATH ]]; then
-    echo "Usage: $0 TESTS_PATH"
+    echo "Usage: $0 TESTS_PATH [PYTORCH_VERSION]"
     exit 1
 fi
 
@@ -17,9 +18,16 @@ else
   PYTORCH="pytorch==$PYTORCH_VERSION"  # Use latest
 fi
 
-source /Users/fabian/Documents/miniconda3/etc/profile.d/conda.sh
-conda create -n torchac_test pip python==3.8 -y
-conda activate torchac_test
+source ~/Documents/miniconda3/etc/profile.d/conda.sh
+
+ENV_NAME=torchac_test
+conda env list | grep $ENV_NAME
+if [[ $? == 1 ]]; then  # Env does not exist yet.
+  conda create -n $ENV_NAME pip python==3.8 -y
+fi
+
+set -e
+conda activate $ENV_NAME
 conda install $PYTORCH torchvision -c pytorch -y
 pip install pytest
 pip install --upgrade torchac --no-cache-dir
